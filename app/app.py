aa270238bc5abf62c567d5ade1736dbdb00cfad7
Usman <usman.bvp@gmail.com>
2024-02-22 22:11:27 +0530

    Revert "Merge pull request #1 from Tejendracse501/main"
    This reverts commit c3743dba7174f4aaeb9c613b0b86a733b144090d, reversing
    changes made to ef42759513ed419e566c32ab22979b5307d85350.

--- app/app.py
+++ app/app.py
@@ -1,14 +1,8 @@
 # Your Flask app routes
-from flask import Flask, render_template, url_for,request
-import pickle
-from sklearn.preprocessing import LabelEncoder
-import pandas as pd
-import numpy as np
-
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__, static_url_path='/static')
-app.debug = True
 
 @app.route('/')
 def home():
@@ -24,7 +18,6 @@ def dashboard():
 def services():
     return render_template('services.html')
 
-
 @app.route('/suitable_market')
 def suitable_market():
     return render_template('suitable_market.html')
@@ -36,7 +29,9 @@ def crop_recommendation():
 def disease_identification():
     return render_template('disease_identification.html')
 
-
+@app.route('/fertilizer_recommendation')
+def fertilizer_recommendation():
+    return render_template('fertilizer_recommendation.html')
 @app.route('/check_weather')
 def check_weather():
     return render_template('check_weather.html')
@@ -67,89 +62,6 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
-
-# ferti-recommendation
-
-@app.route('/fertilizer_recommendation')
-def fertilizer_recommendation():
-    return render_template('fertilizer_recommendation.html')
-
-@app.route('/fertilizer_name', methods=['POST'])
-def fertilizer_name():
-    #FUNCTION to convert categorical values of input_data to numerical values
-    def catergorical_to_num(input_data):
-        numerical_values = input_data
-
-        # Load the label encoders for Soil_Type and Crop_Type
-        soil_type_encoder = LabelEncoder()
-        soil_type_encoder.classes_ = np.array(['Black', 'Clayey', 'Loamy', 'Red', 'Sandy'])
-
-        crop_type_encoder = LabelEncoder()
-        crop_type_encoder.classes_ = np.array(['Barley', 'Cotton', 'Ground Nuts', 'Maize', 'Millets', 'Oil seeds', 'Paddy', 'Pulses', 'Sugarcane', 'Tobacco', 'Wheat'])
-
-        # Encode the categorical values
-        soil_type_encoded = soil_type_encoder.transform([input_data[3]])[0]
-        crop_type_encoded = crop_type_encoder.transform([input_data[4]])[0]
-
-        # Combine numerical and encoded values
-        encoded_data = input_data[:3] + [soil_type_encoded, crop_type_encoded] + list(map(int, input_data[-3:]))
-        return encoded_data
-    
-    #loading pickles
-    # Loading Standard_Scaler
-    with open('../models/standard_scalerFerRecom.pkl', 'rb') as file:
-        scaler = pickle.load(file)
-
-    # Loading the Random Forest Model
-    with open('../models/ferti_recom.pkl', 'rb') as file:
-        model_loaded = pickle.load(file)
-
-    #get the input values from the form
-    input_data=[]
-    input_data.append(float(request.form['Temperature']))
-    input_data.append(float(request.form['Humidity']))
-    input_data.append(float(request.form['Moisture']))
-    input_data.append(request.form['Soil Type'])
-    input_data.append(request.form['Crop Type'])
-    input_data.append(float(request.form['Nitrogen']))
-    input_data.append(float(request.form['Potassium']))
-    input_data.append(float(request.form['Phosphorous']))
-
-    new_data = [catergorical_to_num(input_data)]
-    # Applying Standar_Scaling to new data
-    scaled_data = scaler.transform(new_data)  # Use transform, not fit_transform
-
-    # Predicting for new value with the loaded model
-    prediction = model_loaded.predict(scaled_data)
-
-    # Print the prediction
-    print("Predicted Crop:", prediction)
-
-    # Reversing dictionary of label encodings
-
-    reverse_fertilizer = {0: '10-26-26', 1: '14-35-14', 2: '17-17-17', 3: '20-20', 4: '28-28', 5: 'DAP', 6: 'Urea'}
-    predicted_fertilizer = [reverse_fertilizer[prediction[0]]]
-
-    # print("Predicted Fertilizer: "+predicted_fertilizer[0])
-    return render_template('fertilizer_recommendation.html', recommended=predicted_fertilizer[0])
-
-
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #Backend code for Help page
